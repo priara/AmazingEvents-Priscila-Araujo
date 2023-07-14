42e27d96c349004385b3330bea3371ef57d4e534
priara <priscilaiss16@hotmail.com>
2023-07-14 03:48:25 -0300

    sprint3

--- details.js
+++ details.js
@@ -15,13 +15,13 @@ console.log(contenedorSection);
 
 function crearTarjetaDetails(elementoHTML, evento) {
         elementoHTML.innerHTML += `
-            <div class="fondodiv card mb-3 p-3">
-            <div div class="row g-0 " >
-            <div class="col-md-4 d-sm-flex justify-content-sm-center">
-            <img src="${evento.image}" class="img-fluid rounded-start " alt="...">
+            <div class="fondodivdetails col-12 d-md-flex justify-content-md-center">
+            <div div class="row g-0 col-12 col-md-8 " >
+            <div class="col-md-4 col-10 col-md-10">
+            <img src="${evento.image}" class="img-fluid-details rounded-start col-12 col-md-12 " alt="...">
             </div>
-            <div class="col-md-8">
-            <div class="card-body">
+            <div class="col-md-12 col-12">
+            <div class="card-body-details col-10">
             <h5 class="card-title text-center">${evento.name}</h5>
             <p class="card-text">Date: ${evento.date}</p>
             <p class="card-text ">Description:${evento.description}</p>
